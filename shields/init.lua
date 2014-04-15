10350ebeeded065bb25b74d95481c4b3316a849c
Xanthin <Xanthin@users.noreply.github.com>
2014-04-15 22:39:44 +0200

    added support for intllib translation mod

--- shields/init.lua
+++ shields/init.lua
@@ -1,30 +1,38 @@
+-- Boilerplate to support localized strings if intllib mod is installed.
+local S
+if intllib then
+	S = intllib.Getter()
+else
+	S = function(s) return s end
+end
+
 local use_moreores = minetest.get_modpath("moreores")
 
 -- Regisiter Shields
 
 minetest.register_tool("shields:shield_wood", {
-	description = "Wooden Shield",
+	description = S("Wooden Shield"),
 	inventory_image = "shields_inv_shield_wood.png",
 	groups = {armor_shield=5, armor_heal=0, armor_use=2000},
 	wear = 0,
 })
 
 minetest.register_tool("shields:shield_steel", {
-	description = "Steel Shield",
+	description = S("Steel Shield"),
 	inventory_image = "shields_inv_shield_steel.png",
 	groups = {armor_shield=10, armor_heal=0, armor_use=500},
 	wear = 0,
 })
 
 minetest.register_tool("shields:shield_bronze", {
-	description = "Bronze Shield",
+	description = S("Bronze Shield"),
 	inventory_image = "shields_inv_shield_bronze.png",
 	groups = {armor_shield=10, armor_heal=6, armor_use=250},
 	wear = 0,
 })
 
 minetest.register_tool("shields:shield_diamond", {
-	description = "Diamond Shield",
+	description = S("Diamond Shield"),
 	inventory_image = "shields_inv_shield_diamond.png",
 	groups = {armor_shield=15, armor_heal=12, armor_use=100},
 	wear = 0,
@@ -32,7 +40,7 @@ minetest.register_tool("shields:shield_diamond", {
 
 if use_moreores then
 	minetest.register_tool("shields:shield_mithril", {
-		description = "Mithril Shield",
+		description = S("Mithril Shield"),
 		inventory_image = "shields_inv_shield_mithril.png",
 		groups = {armor_shield=15, armor_heal=12, armor_use=50},
 		wear = 0,
